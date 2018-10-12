a7865e7d2d89830cd9dadb18d644fb850c87d5a3
gaurav <gauravawasthi94506@gmail.com>
2018-10-12 19:26:56 +0530

    some changes made

--- src/main/java/com/citiustech/contact/exceptions/ExceptionHandle.java
+++ src/main/java/com/citiustech/contact/exceptions/ExceptionHandle.java
@@ -1,8 +1,11 @@
 package com.citiustech.contact.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(value=HttpStatus.BAD_REQUEST)
 @ControllerAdvice
 public class ExceptionHandle {
 	
