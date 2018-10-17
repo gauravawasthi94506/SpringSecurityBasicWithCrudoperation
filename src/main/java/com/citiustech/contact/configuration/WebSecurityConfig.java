a4612a54a2426333e3aca49144d475f1edc04afe
gaurav <gauravawasthi94506@gmail.com>
2018-10-17 12:59:36 +0530

    some changes added

--- src/main/java/com/citiustech/contact/configuration/WebSecurityConfig.java
+++ src/main/java/com/citiustech/contact/configuration/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -37,6 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().ignoringAntMatchers("/employees/**")    
                     .and()
                 .logout()
+                	.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                     .permitAll()
                 ;;
     }
